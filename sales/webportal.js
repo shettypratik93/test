bc3c5751dea0b014f00c14faacab38c67cd7d49b
pankajdive <divepankaj1@gmail.com>
2016-02-04 11:04:31 +0530

    new task

---
+++ sales/webportal.js
@@ -0,0 +1,13 @@
+var app = angular.module("portal", ['ngMaterial', 'ngMessages']);
+app.controller("webportal", function($scope) {
+	$scope.types=['Medical shop','grocery','supermarket','other'];
+	 // $urlRouterProvider.otherwise('/home');
+    
+  //   $stateProvider//,$stateProvider, $urlRouterProvider//, 'ui.router'
+        
+  //       // HOME STATES AND NESTED VIEWS ========================================$stateProvider, $urlRouterProvider
+  //       .state('home', {
+  //           url: '/home',
+  //           templateUrl: 'webportal1.html'
+  //       })
+});
