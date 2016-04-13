5efac01c1d84d9d9cfd783614eb72b354528fa90
shettypratik93 <shettypratik93@gmail.com>
2016-04-13 11:57:27 +0530

    Working on things

---
+++ Alpha/task2.js
@@ -0,0 +1,12 @@
+(function() {
+    var app = angular.module("task2", ["ngMaterial", 'ngMessages','ui-router']);
+
+    app.controller("formmm", function($scope) {
+        $scope.checknum = function(item) {
+
+            
+        }
+
+    });
+
+})();
